3d0587c3cd9e96d8b7bc50eaa3ff86ae61ed128b
Arturo <Arturo@DESKTOP-L6C1D74>
2017-12-10 21:29:58 -0500

    Got player to network, but names don't show up

---
+++ Project_3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    public List<Player> players;
+    public List<string> playersName;
+
+	void Start () {
+	    
+	}
+	
+	void Update () {
+	    
+	}
+}
---
+++ Project_3/Assets/Scripts/Player.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public class PlayerData
+{
+    public string myName;
+    public GameObject myObject;
+
+    public PlayerData(string n, GameObject g)
+    {
+        myName = n;
+        myObject = g;
+    }
+
+    public PlayerData()
+    {
+
+    }
+}
+
+public class Player : NetworkBehaviour {
+
+    [SerializeField]
+    private GameObject myCanvas;
+
+    [SyncVar]
+    public string myName = "PLAYER";
+
+    [SerializeField]
+    Text nameText;
+
+    [SerializeField]
+    private float speed = 2f;
+
+    [SerializeField]
+    RectTransform myTransform;
+
+    [SerializeField]
+    private GameManager gameManager;
+
+    PlayerData myData;
+
+    void Start()
+    {
+        myCanvas = GameObject.FindGameObjectWithTag("PlayerCanvas");
+        gameManager = FindObjectOfType<GameManager>();
+
+        if (isLocalPlayer)
+        {
+            //CmdPlayerHasJoined(myData);
+            GetComponent<Image>().color = Color.blue;
+            nameText.color = Color.green;
+        }
+        else
+            nameText.color = Color.red;
+
+        nameText.text = myName;
+
+        transform.SetParent(myCanvas.transform);
+    }
+
+    //[Command]
+    //public void CmdPlayerHasJoined(PlayerData myData)
+    //{
+    //    RpcPlayerHasJoined(myData);
+    //    gameManager.players.Add(this);
+    //    gameManager.playersName.Add(myName);
+    //}
+
+    //[ClientRpc]
+    //public void RpcPlayerHasJoined(PlayerData myDataa)
+    //{
+    //    myName = myDataa.myName;
+    //    nameText.text = myName;
+    //    gameManager.players.Add(this);
+    //    gameManager.playersName.Add(myName);
+    //}
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        myTransform.Translate(move * speed * Time.deltaTime);
+    }
+
+}
